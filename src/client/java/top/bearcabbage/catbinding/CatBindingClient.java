e1a90912acb3ec428135df0dd5304cad71f0a9ab
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-06-11 23:27:15 +0800

    Initialize.

---
+++ src/client/java/top/bearcabbage/catbinding/CatBindingClient.java
@@ -0,0 +1,10 @@
+package top.bearcabbage.catbinding;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CatBindingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
